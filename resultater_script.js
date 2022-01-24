9d965415804e088798fe16ddd710a6e13b1804f7
599029-Iver <89256936+599029-Iver@users.noreply.github.com>
2022-01-24 16:05:00 +0100

    Add files via upload

---
+++ resultater_script.js
@@ -0,0 +1,10 @@
+
+document.getElementsByClassName("bilderSpill").addEventListener("click", visInfo);
+
+function visInfo(){
+    document.getElementById("skriv").innerHTML= 5 + 6;
+}
+
+function lastSide(){
+    
+}
